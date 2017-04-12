a87c5ada097af802a9db1a1c45f378a869747ccf
alaizaz <aizaz.ali@hotmail.com>
2017-04-12 17:35:17 +0100

    Update home.js

--- js/home.js
+++ js/home.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	de = document.documentElement;
 	setHeight();
 	setWidth();	
-/*	$('.arrow-left').on('click', function(e){
+	$('.arrow-left').on('click', function(e){
     	e.preventDefault();
     		var swiper = $(this).siblings('.swiper-container').data('swiper');
     			swiper.swipePrev();
@@ -42,7 +42,7 @@ $(window).load(function() {
 	    loop:true,
 	 /*   speed:800,*/
 	    useCSS3Transforms:true,
-	/*    grabCursor: true,*/
+	    grabCursor: true,*/
 	    paginationClickable: true,
 	   /* autoplay: 10000*/
 	});
