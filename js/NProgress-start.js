eea8bdc51a1c63562162aefd8135eebdc1657155
alaizaz <aizaz.ali@hotmail.com>
2017-06-06 17:24:12 +0100

    Delete NProgress-start.js

--- js/NProgress-start.js
+++
@@ -1,9 +0,0 @@
-$('.main_wrap').show();
-NProgress.start();
-
-$(window).load(function() {
-  // Animate loader off screen
-  NProgress.done();
-  setTimeout(function() { $('.hider').fadeOut(); }, 600);
-});
-$('.blank').addClass('hider');
